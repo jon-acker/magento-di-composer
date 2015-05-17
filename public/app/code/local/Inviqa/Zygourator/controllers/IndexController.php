da8e54d1c627784340a9a72d43f7b6326586f3d7
Jon Acker <jacker@inviqa.com>
2015-05-17 21:05:17 +0100

    Added example for defining services in both xml and yaml

--- public/app/code/local/Inviqa/Zygourator/controllers/IndexController.php
+++ public/app/code/local/Inviqa/Zygourator/controllers/IndexController.php
@@ -5,7 +5,7 @@ class Inviqa_Zygourator_IndexController extends Inviqa_SymfonyContainer_Controll
     public function indexAction()
     {
         $this->_container->get('acme.product.catalog')->findAll();
-        
+
         $this->loadLayout();
         $this->renderLayout();
     }
